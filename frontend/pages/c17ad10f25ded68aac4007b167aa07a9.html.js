2a4cd52de237927d59351fe99cd1b372072ae5e3
Adam Gold <adamgold7@gmail.com>
2021-03-21 19:00:33 +0200

    feat: add html page for sendinblue verification

---
+++ frontend/pages/c17ad10f25ded68aac4007b167aa07a9.html.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layout'
+import React from 'react'
+
+export default class Sendinblue extends React.Component {
+    render = () => {
+        return (
+            <Layout>
+
+            </Layout >
+        )
+    }
+}
